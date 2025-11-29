8ac094daff45dd17eb2d8c85d8e0b5f189ff5d1a
AndikaAviaLukita <225802229+AndikaAviaLukita@users.noreply.github.com>
2025-11-30 01:43:17 +0700

    middleware admin dan route grouping

--- Sistem-manajemen-tugas-tim/routes/web.php
+++ Sistem-manajemen-tugas-tim/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +11,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
